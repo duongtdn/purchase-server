1c8ae5b594f3c605da0bd0af38fa8b140376d63c
duongtdn <duongtdn@gmail.com>
2018-08-12 15:01:47 +0000

    first commit

---
+++ example/server.local.js
@@ -0,0 +1,14 @@
+"use strict"
+
+require('dotenv').config()
+
+const db = require('database-test-helper')
+const invoicedb = require('invoicedb-test-helper')
+
+db.start().add({invoicedb}).init(() => {
+  // const app = require('./app.local')
+  const PORT = process.env.PORT_LOCAL_PURCHASE || 3210;
+  // const httpServer = require('http').createServer(app);
+  // httpServer.listen(PORT)
+  console.log(`\n# PURCHASE-SERVICES is running at http://localhost:${PORT}\n`);
+});
